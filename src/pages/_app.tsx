da05005731fca1ec9011ac1db23a477eeb843b47
Ignacio Diaz <diaz.ignacio023@gmail.com>
2023-01-04 19:49:50 -0300

    finish tutorial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
-import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
+import AuthProvider from "../components/AuthProvider";
+
+import "../styles/globals.css";
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThirdwebProvider>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,58 @@
 import type { NextPage } from "next";
+import {
+  useActiveListings,
+  useContract,
+  useAddress,
+} from "@thirdweb-dev/react";
+import NFTCard from "../components/NFTCard";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <div>TODO</div>;
+  const address = useAddress();
+  const { contract } = useContract(
+    "0x4cD6B34B855B165E87005781C347C1FCe64c1847",
+    "marketplace"
+  );
+
+  const { data: nfts, isLoading } = useActiveListings(contract);
+
+  if (isLoading)
+    return (
+      <div className={"mb-3 flex w-screen justify-center"}>Loading ...</div>
+    );
+
+  return (
+    <div className="space-y-4 p-2">
+      <div className="text-2xl font-semibold">Active Listings</div>
+      <Link href={`profile/${address}`}>
+        <div className={"cursor-pointer text-2xl font-semibold"}>
+          My Collections
+        </div>
+      </Link>
+
+      <div className="nft-grid">
+        {nfts &&
+          nfts.map((nft) => {
+            return (
+              <Link
+                href={`/assets/${nft.id}`}
+                key={nft.assetContractAddress + nft.id}
+              >
+                <a>
+                  <NFTCard
+                    nft={{
+                      name: nft.asset.name as string,
+                      tokenUri: nft.asset.image as string,
+                      price: nft.buyoutCurrencyValuePerToken?.displayValue,
+                    }}
+                  />
+                </a>
+              </Link>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
